9eb01e1582d571580d42cd3866a86efd59885a7e
KizunaMo <mo791206@gmail.com>
2021-10-20 11:38:51 +0800

    Practice

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int posion = 0b_0010;
+            int status = 0;
+
+            status = status | posion;
+            string status2 = Convert.ToString(status, 2);
+            Console.WriteLine(status2);
+
+            status = status & (~posion);
+            status2 = Convert.ToString(status, 2);
+            Console.WriteLine(status2);
+
+        }
+    }
+}
---
+++ Interface Strategy Design Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interface_Strategy_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
