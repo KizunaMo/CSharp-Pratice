fcf465bc80f2a129d829b633263421225f78fa81
KizunaMo <mo791206@gmail.com>
2021-10-23 19:56:52 +0800

    練習用程式算三角函數:用已知的夾角角度，求出對應的XY座標

---
+++ Vector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace VectorPractice
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            //練習 向量計算
+            Vector3 a;
+            Vector3 b;
+
+            Practice1();
+            
+            
+
+        }
+
+        static public void Practice1()
+        {
+            //假設知道單位圓夾角 求三角形一邊頂點座標(X,Y)
+            //角度轉弧度var radian = (angle * Math.PI) / 180;
+            Console.WriteLine($"輸入夾角");
+            string cmd = Console.ReadLine();
+            float cmdAngle;
+            if (float.TryParse(cmd, out cmdAngle))
+            {
+                float radian = cmdAngle * MathF.PI / 180;
+                Vector2 xy = new Vector2(MathF.Cos(radian),MathF.Sin(radian));
+                Console.WriteLine($"單位圓XY座標{xy}\nX=cosΘ(長度){xy.X}\nY=sinΘ(長度){xy.Y}");
+            }
+
+
+
+        }
+    }
+}
