6e74d55f80ca115204b2d9edb884db2a3a7f9102
KizunaMo <mo791206@gmail.com>
2021-11-15 19:54:45 +0800

    試著跟著寫 Queue

---
+++ Algorithms/MyQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class MyQueue<DT>
+    {
+        MyLinkedList<DT> list = new MyLinkedList<DT>();
+
+        public void EnQueue(DT data)
+        {
+            list.Append(data);
+        }
+
+        public DT DeQueue()
+        {
+            var node = list.First();
+            if (node == null)
+                throw new Exception();
+            return node.data;
+        }
+
+    }
+}
